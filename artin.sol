1e03736aecd6995eb47efd9526eb24c1cb9dde1d
Artin Rahmati <106176909+xxArtinxx@users.noreply.github.com>
2022-05-29 03:47:52 +0430

    Update artin.sol

--- artin.sol
+++ artin.sol
@@ -11,14 +11,10 @@ contract artin {
     }
     
     mapping(address => userdata) private _UsersData;
-
-
+    
     function yourName(string memory _ownerWallet) public {
         _UsersData[msg.sender].ownerWallet = _ownerWallet;
     }
-
-    
-
     function showAdressWallet() public view returns(string memory){
         return walletAddress;
     } 
