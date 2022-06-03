a03e6fdabfb1e2308cd180ad01423b5a986e8d44
Artin Rahmati <106176909+xxArtinxx@users.noreply.github.com>
2022-06-03 17:26:15 +0430

    Update artin.sol

--- artin.sol
+++ artin.sol
@@ -1,21 +1,39 @@
 //jalase 2 class solidity
 
-pragma solidity 0.8.14;
+pragma solidity 0.8.7;
+
 
 contract artin {
 
-    string walletAddress = "0x62ascjaj412jkA78225tqfaGFASCASFHGr";
+   
 
-    struct userdata{
-        string ownerWallet;
+   struct ETHpayAmount{
+        uint Amount;   
+        bool isPaid; 
+     }
+ 
+    mapping(address=>ETHpayAmount)private _ETHpay;
+
+    //  string private err = 'its low Amount. please input Amount Above 1';
+
+    function Payment (uint _Amount) public{
+        if(_Amount <= 3 ){
+            //   _ETHpay[msg.sender].Amount = _err;
+             _ETHpay[msg.sender].isPaid = false ;   
+        }
+        else{
+            _ETHpay[msg.sender].Amount = _Amount ;
+            _ETHpay[msg.sender].isPaid = true ; 
+        }
     }
-    
-    mapping(address => userdata) private _UsersData;
-    
-    function yourName(string memory _ownerWallet) public {
-        _UsersData[msg.sender].ownerWallet = _ownerWallet;
+
+
+    function PaymentAmount() public view returns(uint){
+        return _ETHpay[msg.sender].Amount;
     }
-    function showAdressWallet() public view returns(string memory){
-        return walletAddress;
-    } 
+
+    function PaymentStatus() public view returns(bool){
+        return _ETHpay[msg.sender].isPaid;
+    }
+
  }
